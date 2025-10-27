b6e52896b3d511d625cbe52450afea28ae4450c7
BootcampAI <57165250+Bootcamp-AI@users.noreply.github.com>
2025-10-27 10:33:22 -0500

    Add header

---
+++ excercise.cpp
@@ -0,0 +1,21 @@
+// In the following code example, the functions are out of order, and the code will not compile. 
+// Try to fix this by rearranging the functions to be in the correct order. 
+  
+  #include <iostream>
+  using std::cout;
+
+  void OuterFunction(int i) 
+  {
+      InnerFunction(i);
+  }
+
+  void InnerFunction(int i) 
+  {
+      cout << "The value of the integer is: " << i << "\n";
+  }
+
+  int main() 
+  {
+      int a = 5;
+      OuterFunction(a);
+  }
